789e7997ec0b60d6ce13ee1fbc859414f60badc4
Itzzyee47 <111084815+Itzzyee47@users.noreply.github.com>
2024-09-05 09:17:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,7 @@ def main(page: ft.Page):
     page.padding = 0
     
      
-    wv = ft.WebView(
-        "https://landmarkai.onrender.com/",
-        width=500,height=650,
-        on_page_started=lambda _: print("Page started"),
-        on_page_ended=lambda _: print("Page ended"),
-        on_web_resource_error=lambda e: print("Page error:", e.data),
-    )
+    wv = ft.Text('WebView was the problem')
     sa = ft.SafeArea(content=wv,)
     page.add(sa,)
 
