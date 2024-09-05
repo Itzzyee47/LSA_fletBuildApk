ed8ee96848448817d82a6f85ca365ef267c6fbd1
Itzzyee47 <111084815+Itzzyee47@users.noreply.github.com>
2024-09-05 09:01:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def main(page: ft.Page):
      
     wv = ft.WebView(
         "https://landmarkai.onrender.com/",
-        expand=True,
+        width=500,height=650,
+        on_page_started=lambda _: print("Page started"),
+        on_page_ended=lambda _: print("Page ended"),
+        on_web_resource_error=lambda e: print("Page error:", e.data),
     )
     sa = ft.SafeArea(content=wv,)
     page.add(sa,)
