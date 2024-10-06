690675bb2832e595d1ddafa48d306ee9391fdd7e
Itzzyee47 <111084815+Itzzyee47@users.noreply.github.com>
2024-10-06 15:01:28 -0700

    Updated main.py content to add website and build as apk

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import flet as ft
 def main(page: ft.Page): 
     page.padding = 0
     
-     
-    wv = ft.Text('WebView was the problem')
-    sa = ft.Container(content=wv,)
+    # Create a WebView to load a website
+    web_view = ft.WebView(
+        src="https://landmarkai.onrender.com/",
+        expand=1,
+    )
+    #wv = ft.Text('WebView was the problem')
+    sa = ft.SafeArea(content=web_view,expand=True,)
     page.add(sa)
 
 ft.app(main)
